2c2b39cc66353383eaca6b4c7840f5a21ddee1c1
Leonidas <ldbrooks297@gmail.com>
2021-02-05 17:16:26 -0800

    working with cloudinary to upload and display an image

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 const ejsLayouts = require('express-ejs-layouts')
 const multer = require('multer')
 const cloudinary = require('cloudinary')
+const { render } = require('ejs')
+const uploads = multer({ dest: './uploads'})
 
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
@@ -12,6 +14,23 @@ app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req,res)=>{
     res.send('express cloudinary')
+})
+
+app.get('/images', (req,res)=>{
+    res.render('index')
+})
+
+app.get('/images/new', (req,res)=>{
+    res.render('new')
+})
+
+app.post('/images', uploads.single('inputFile'), (req,res)=>{
+    const image = req.file.path
+    console.log(image)
+    cloudinary.uploader.upload(image, (result) =>{
+        console.log(result)
+        res.render('index', { image: result.url })
+    })
 })
 
 const PORT = process.env.PORT || 8888
